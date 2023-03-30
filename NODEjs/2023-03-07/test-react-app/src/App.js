bcd8d80c2d6087e736c8fd70d609cdfb9c0de88d
NerijusBalez <117636348+Liutazbn82@users.noreply.github.com>
2023-03-30 15:08:22 +0300

    PVM skaiciuokle

--- NODEjs/2023-03-07/test-react-app/src/App.js
+++ NODEjs/2023-03-07/test-react-app/src/App.js
@@ -1,102 +1,157 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
-
-// function App() {
-//   const [name, setName] = useState("Putka")
-
-//   return (
-//     <div className="App">  
-//       <header className="App-header">
-//         <div>
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <div id="name">{name}</div>
-//           <button onClick={ () => {setName("Petras")  }}>Keisti reiksme!</button>
-//         </div> 
-//       </header> 
-//     </div>
-//   );
-// }
-
-// function App() {
-//   const [count, setCount] = useState(0)
-//   console.log(count)
-//   return (
-//     <div className="App">  
-//       <header className="App-header">
-//         <div>
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h1>{count}</h1>
-
-//           <button onClick={ () => {
-//             setCount(count + 1)
-//           }}>Didinti!</button>
-
-//           <button onClick={ () => {
-//             setCount(count - 1)
-//           }}>Mazinti!</button>   
-
-//         </div> 
-//       </header> 
-//     </div>
-//   );
-// }
-
-// function App() {
-//   const [inputName, setInputName] = useState("")
-//   console.log(inputName)
-//   return (
-//     <div className="App">  
-//       <header className="App-header">
-//         <div>
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <br/>
-//           <input
-//               type="textas"
-//               placeholder="Name..."
-//               onChange={(event) => {
-//                 setInputName(event.target.value);
-//               }} 
-//           />
-//           <h1>{inputName}</h1>
-
-
-//         </div> 
-//       </header> 
-//     </div>
-//   );
-// }
+import React, { 
+  // useState, 
+  // useRef, 
+  // useEffect 
+} from 'react';
+// import Skaiciuokle from './Skaiciuokle'
+// https://www.pluralsight.com/guides/handling-multiple-inputs-with-single-onchange-handler-react
 
 function App() {
-  // galima naudoti paprastai (eilute zemiau)
-  // const counterState = useState(0)
-  // console.log(counterState[0])
-  // console.log(counterState[1])
 
-  // bet pakeisime į kitoki buda
-  const [counter, setCounter] = useState(0)
+  const [ state, setState ] = React.useState({
+    pvmDydis: 0.21,
+    sumaBePvm: 0,
+    pvmSuma: 0,
+    sumaSuPvm: 0
+  })
 
-  function increment() {
-    // setCounter(counter + 1)
-    setCounter(( prevCounter ) => {
-      return prevCounter + 3
-    })
+  function handleChange(event) {
+    event.preventDefault();
+    const value = event.target.value;
+    const newData = {
+      ...state,
+      [event.target.name]: value
+    };
+    
+    if(event.target.name === 'sumaBePvm')
+      newData.sumaSuPvm = +newData.sumaBePvm + (+newData.sumaBePvm * newData.pvmDydis);
 
-    setCounter(prev => prev + 1 )
+    if(event.target.name === 'sumaSuPvm')
+      newData.sumaBePvm = +newData.sumaSuPvm / (1 + +newData.pvmDydis);
+
+      newData.pvmSuma = (+newData.sumaBePvm * +newData.pvmDydis);
+
+    setState(newData);
+
+    console.log(newData);
   }
 
-  function decrement() {
-    setCounter(counter - 1)
-  }
+  
+
+// *****************************************************************
+  // function clickToCount(e) {
+  //   e.preventDefault();
+  //   // console.log('mygtukas skaiciuoti suveikia paspaudus')
+  //   const form = e.target;
+  //   const formData = new FormData(form);
+
+  //   // You can pass formData as a fetch body directly:
+  //   // fetch('/some-api', { method: form.method, body: formData });
+
+  //   // Or you can work with it as a plain object:
+  //   const formJson = Object.fromEntries(formData.entries());
+  //   console.log('skaičiai iš formos', formJson);
+  //   // console.log('skaičiai iš formos', formJson.sumaBePvm, formJson.pvmDydis);
+  //   const sumaBePvm = formJson.sumaBePvm;
+  //   const pvmDydis = formJson.pvmDydis;
+
+  //   console.log('bendra suma su pvm', (sumaBePvm *(pvmDydis/100)));
+  // }
+// ********************************************************************
 
   return (
-    <div>
-      <h1>Skaitiklis: {counter}</h1>
-      <button onClick={increment}>Prideti</button>
-      <button onClick={decrement}>Atimti</button>
-    </div>
-  )
+    <>
 
+      <div className="App">
+
+        <h1 className="mb-3">PVM skaičiuoklė</h1>
+
+        {/* <form onSubmit={clickToCount} className="form-control w-50 bg-success form"> */}
+
+        <form 
+          // onChange={handleChangeSumaBePvm} 
+          className="form-control w-50 bg-success form"
+          >
+          <div className="container">
+            <div className="input-group mb-3 mt-3">
+              <span className="input-group-text bg-warning col-4">PVM dydis</span>
+              <select 
+                className="form-select text-center" 
+                name="pvmDydis" 
+                id="pvmDydis"
+                type="number"
+                onChange={handleChange}
+              >
+                <option value="0.21">21%</option>
+                <option value="0.19">19%</option>
+                <option value="0.09">9%</option>
+                <option value="0.05">5%</option>
+              </select>
+            </div>
+
+            <div className="input-group mb-3">
+              <span 
+                  className="input-group-text bg-info-subtle col-4 " 
+                  id="inputGroup-sizing-default">Suma be PVM
+              </span>
+              <input 
+                  name="sumaBePvm" 
+                  value={state.sumaBePvm} 
+                  onChange={handleChange} 
+                  id="sumaBePvm" 
+                  type="number" 
+                  className="form-control text-center" 
+                  aria-label="Sizing example input" 
+                  aria-describedby="inputGroup-sizing-default" 
+              />
+            </div>
+
+            <div className="input-group mb-3">
+              <span 
+                  className="input-group-text bg-warning-subtle col-4" 
+                  id="inputGroup-sizing-default">PVM suma
+              </span>
+              <input 
+                  name="pvmSuma" 
+                  value={state.pvmSuma}
+                  id="pvmSuma" 
+                  type="number" 
+                  className="form-control text-center" 
+                  aria-label="Sizing example input" 
+                  aria-describedby="inputGroup-sizing-default" 
+                  // onChange={handleChange}
+                  disabled 
+              />
+            </div>
+
+            <div className="input-group mb-3">
+              <span 
+                  className="input-group-text bg-warning-subtle col-6" 
+                  id="inputGroup-sizing-default">Bendra suma (su PVM)
+              </span>
+              <input 
+                  name="sumaSuPvm" 
+                  onChange={handleChange}
+                  value={state.sumaSuPvm}
+                  id="sumaSuPvm" 
+                  type="number" 
+                  className="form-control text-center fw-bold" 
+                  aria-label="Sizing example input" 
+                  aria-describedby="inputGroup-sizing-default" 
+              />
+            </div>
+            {/* <button
+              type="submit"
+              className="btn btn-warning mt-1 mb-3"
+              onClick={handleChange2}
+              >Skaičiuoti!
+            </button> */}
+          </div>
+        </form>
+      </div>
+
+    </>
+  )
 }
 
 export default App;
